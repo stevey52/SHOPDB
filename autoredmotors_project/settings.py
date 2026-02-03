ac4e9675a98f7078ea056ddd3964152a81a37b94
stevey52 <49789919+stevey52@users.noreply.github.com>
2026-02-03 22:22:16 +0300

    Set DEBUG to False and update ALLOWED_HOSTS

--- autoredmotors_project/settings.py
+++ autoredmotors_project/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-cfm!b$44=c4l1b1_sne9jhez070rf4ag0e^2qms_vmb3x^_s(!'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['https://www.pythonanywhere.com/user/mtawa/']
 
 
 # Application definition
